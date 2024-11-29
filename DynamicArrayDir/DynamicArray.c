4dd750aaf595b9b7d2d8c94dc80ef452c3ec5c36
kwal <kaiwalker0731@gmail.com>
2024-11-30 01:11:06 +1100

    Dynamic Array initialisation

--- DynamicArrayDir/DynamicArray.c
+++ DynamicArrayDir/DynamicArray.c
@@ -1,4 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dynamic_array {
+int size;
+void *arr_pointer;
+// TODO: arr_pointer can be changed when resizing
+} dynamic_array;
+
+struct dynamic_array initialize_dynamic_arr() {
+struct dynamic_array *dynamic_arr_ptr = malloc(8);
+void *void_arr[8];
+struct dynamic_array dynamic_arr = {.size = 0, .arr_pointer = &void_arr};
+return dynamic_arr;
+}
+
 int main() {
+
+struct dynamic_array dyn_arr = initialize_dynamic_arr();
+printf("%d\n", dyn_arr.size);
 
 return 0;
 }
